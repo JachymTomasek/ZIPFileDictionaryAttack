c06512b7d515a64ac088705757f0ee7f940b5bc5
Jáchym Tomášek <87898573+JachymTomasek@users.noreply.github.com>
2022-01-15 11:50:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import pyzipper
+
+zip_path = input("ZIP file path: ")
+dictionary_path = input("Dictionary TXT file path: ")
+
+with open(dictionary_path, "r", encoding="utf-8") as f:
+
+    correct_pwd = False
+    for row in f.readlines():
+        if correct_pwd == False:
+            string_pwd = row.strip()
+            print(string_pwd)
+
+            my_password = bytes(string_pwd, encoding="utf8")
+            print(my_password)
+
+            with pyzipper.AESZipFile(zip_path) as my_zip:
+
+                try:
+                    my_zip.extractall(pwd=my_password)
+                    print("spravne", string_pwd)
+                    correct_pwd = True
+                except Exception as e:
+                    print("spatne")
+                    correct_pwd = False
+        else:
+            break
+
+
+
+
+
